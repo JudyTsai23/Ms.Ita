7db82012cba96f80183b8bf5a3938761bcc981b2
Judy_Tsai <ydusai0103@gmail.com>
2021-09-29 17:10:33 +0800

    [YC] 重整專案架構，新增sass功能設定，完成Header雛形

---
+++ src/components/containers/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <b-navbar class="nav-bar" toggleable="md" type="dark">
+      <b-navbar-brand to="/home">
+        <img class="logo" src="../../assets/img/logo/logo.png" alt="Ms.Ita" />
+      </b-navbar-brand>
+      <!-- <b-navbar-brand to="/home">Ms. Ita</b-navbar-brand> -->
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item class="mr-2" to="/home">首頁</b-nav-item>
+          <b-nav-item class="mr-2">訊息專區</b-nav-item>
+          <b-nav-item-dropdown class="mr-2" text="美味餐點" right>
+            <b-dropdown-item variant="secondary">主菜</b-dropdown-item>
+            <b-dropdown-item variant="secondary">沙拉</b-dropdown-item>
+            <b-dropdown-item variant="secondary">甜點</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item class="mr-2">訂位</b-nav-item>
+          <b-nav-item class="mr-2">訂餐</b-nav-item>
+          <b-nav-item class="mr-2">關於我們</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+<script>
+export default {
+  name: "Header",
+};
+</script>
+<style scoped>
+.nav-bar {
+  background-color: #343a40f1;
+}
+.logo {
+  height: 50px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+// Container
+const Container = () => import("../components/containers/Container.vue");
+// Home
+const Home = () => import("../views/Home/Home.vue");
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: "hash",
+  linkActiveClass: "active",
+  scrollBehavior: () => ({ y: 0 }),
+  routes: configRoutes(),
+});
+
+export default router;
+
+function configRoutes() {
+  return [
+    {
+      path: "/",
+      redirect: "/home",
+      component: Container,
+      children: [
+        {
+          path: "home",
+          name: "Home",
+          component: Home,
+        },
+      ],
+    },
+  ];
+}

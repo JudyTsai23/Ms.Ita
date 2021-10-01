e709a0809ebecc511b7dc4f8377922a8e3241373
Judy_Tsai <ydusai0103@gmail.com>
2021-10-01 11:35:24 +0800

    [YC] 完成 footre、header

--- src/components/containers/Footer.vue
+++ src/components/containers/Footer.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="footer">
-    <b-row align-v="center" align-h="center" class="text-center">
-      <b-col cols="2" align-self="start">
-        <img class="logo" src="../../assets/img/logo/logo.png" />
-      </b-col>
-      <b-col cols="1" align-self="start" class="title">
-        <router-link to="/home">首頁</router-link></b-col
-      >
-      <b-col cols="1" align-self="start" class="title">訊息專區</b-col>
-      <b-col cols="1" align-self="start" class="title">美味餐點</b-col>
-      <b-col cols="1" align-self="start">
-        <b-row>
-          <b-col class="title">線上預訂</b-col>
-        </b-row>
-        <b-row>
-          <b-col class="item">訂位</b-col>
-        </b-row>
-        <b-row>
-          <b-col class="item">訂餐</b-col>
-        </b-row>
-      </b-col>
-      <b-col cols="2" align-self="start" class="title">關於我們</b-col>
-    </b-row>
+    <div class="footer-inside">
+      <b-row align-v="center" align-h="center" class="text-center">
+        <b-col cols="2" align-self="start">
+          <img class="logo" src="../../assets/img/logo/logo.png" />
+        </b-col>
+        <b-col md="2" align-self="start" class="title">
+          <router-link to="/home">首頁</router-link></b-col
+        >
+        <b-col md="2" align-self="start" class="title">訊息專區</b-col>
+        <b-col md="2" align-self="start" class="title">美味餐點</b-col>
+        <b-col md="2" align-self="start">
+          <b-row>
+            <b-col class="title mb-1">線上預訂</b-col>
+          </b-row>
+          <b-row>
+            <b-col class="item">訂位</b-col>
+          </b-row>
+          <b-row>
+            <b-col class="item mb-1">訂餐</b-col>
+          </b-row>
+        </b-col>
+        <b-col md="2" align-self="start" class="title">關於我們</b-col>
+      </b-row>
+    </div>
   </div>
 </template>
 <script>
@@ -30,12 +32,6 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-@mixin router-link($link-color) {
-  color: $link-color;
-  text-decoration: none;
-  cursor: pointer;
-}
-
 .footer {
   position: fixed;
   left: 0;
@@ -47,23 +43,28 @@ export default {
   color: white;
   padding-top: 3vh;
   font-size: 12px;
-  .logo {
-    height: 100px;
-  }
-  .title,
-  a {
-    @include router-link(rgb(111, 131, 111));
-    font-size: 16px;
-    margin-bottom: 5px;
-  }
-  .title:hover,
-  a:hover,
-  .item:hover {
-    @include router-link(rgb(136, 167, 136));
-  }
-  .item {
-    font-weight: normal;
-    @include router-link(rgb(111, 131, 111));
+
+  .footer-inside {
+    width: 75%;
+    margin: auto;
+
+    .logo {
+      height: 100px;
+    }
+    .title,
+    a {
+      @include router-link(rgb(111, 131, 111));
+      font-size: 16px;
+    }
+    .title:hover,
+    a:hover,
+    .item:hover {
+      @include router-link(rgb(136, 167, 136));
+    }
+    .item {
+      font-weight: normal;
+      @include router-link(rgb(111, 131, 111));
+    }
   }
 }
 </style>
--- src/components/containers/Header.vue
+++ src/components/containers/Header.vue
@@ -12,13 +12,15 @@
         <b-navbar-nav class="ml-auto mr-auto">
           <b-nav-item class="mr-5" to="/home">首頁</b-nav-item>
           <b-nav-item class="mr-5">訊息專區</b-nav-item>
-          <b-nav-item-dropdown class="mr-5" text="美味餐點" right>
+          <b-nav-item-dropdown class="mr-5 dropdown-list" text="美味餐點">
             <b-dropdown-item variant="secondary">主菜</b-dropdown-item>
             <b-dropdown-item variant="secondary">沙拉</b-dropdown-item>
             <b-dropdown-item variant="secondary">甜點</b-dropdown-item>
           </b-nav-item-dropdown>
-          <b-nav-item class="mr-5">訂位</b-nav-item>
-          <b-nav-item class="mr-5">訂餐</b-nav-item>
+          <b-nav-item-dropdown class="mr-5" text="線上預訂">
+            <b-dropdown-item>訂位</b-dropdown-item>
+            <b-dropdown-item>訂餐</b-dropdown-item>
+          </b-nav-item-dropdown>
           <b-nav-item class="mr-5">關於我們</b-nav-item>
         </b-navbar-nav>
       </b-collapse>

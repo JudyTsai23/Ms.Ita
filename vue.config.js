a788e0d4deb55128c6ca63758f737f385aca05df
joyu <joyuhuang@gmail.com>
2021-10-06 16:41:03 +0800

    [Joyu] 修正scss引入方式

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,30 @@
 module.exports = {
-  lintOnSave: false,
-  runtimeCompiler: true,
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `@import "@/assets/scss/style.scss";`,
-      },
-    },
-  },
-  // configureWebpack: {
-  //   resolve: {
-  //     symlinks: false,
-  //   },
-  // },
-  // devServer: {
-  //   host: "localhost",
-  //   port: 8080,
-  //   https: true,
-  //   proxy: {
-  //     "^/ess/": {
-  //       target: "https://jeff.syscom.com:8081/",
-  //       changeOrigin: true,
-  //       secure: false,
-  //     },
-  //   },
-  // },
+	lintOnSave: false,
+	runtimeCompiler: true,
+	css: {
+		loaderOptions: {
+			sass: {
+				prependData: `
+					@import "@/assets/scss/_custom-settings.scss";
+        `,
+			},
+		},
+	},
+	// configureWebpack: {
+	//   resolve: {
+	//     symlinks: false,
+	//   },
+	// },
+	// devServer: {
+	//   host: "localhost",
+	//   port: 8080,
+	//   https: true,
+	//   proxy: {
+	//     "^/ess/": {
+	//       target: "https://jeff.syscom.com:8081/",
+	//       changeOrigin: true,
+	//       secure: false,
+	//     },
+	//   },
+	// },
 };

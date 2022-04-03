0015b1dd63a164319d8f0d881b0648afad44d1dc
YC <saiyuki0804@gmail.com>
2022-04-03 16:26:28 +0800

    [YC] 調整首頁版面、串接後端

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -17,48 +17,42 @@ export default {
         { label: "親切的服務態度", pic: "restaurant/service.jpg", link: "#" },
         { label: "專業的服務態度", pic: "other/coffee.png", link: "#" },
       ],
-      // 訊息專區
-      msgArr: [
-        {
-          title: "[活動]",
-          content: "當月壽星打卡即可獲得精美小禮!",
-          link: "#",
-        },
-        { title: "[活動]", content: "雙十國慶優惠活動開始囉!", link: "#" },
-        {
-          title: "[活動]",
-          content: "五倍券優惠禮，消費滿300折50元!",
-          link: "#",
-        },
-        {
-          title: "[公告]",
-          content: "即日起營業時間小幅修改，詳情請點連結。",
-          link: "#",
-        },
-        {
-          title: "[活動]",
-          content: "打卡送飲品! 凡消費加打卡，tag店家帳號，免費送一杯飲品! 不限金額品項，有打有送!",
-          link: "#",
-        },
-      ],
+      // 訊息清單
+      newsList: [],
     };
   },
   mounted() {
-    // this.$store.commit("isShowLoading", true);
-    // setTimeout(() => {
-    //   // 測試用
-    //   this.$store.commit("isShowLoading", false);
-    // }, 5000);
+    this.init();
   },
   methods: {
-    serverTest() {
-      AjaxService.get(
-        "/server/test",
+    init() {
+      this.getHomeNewsList();
+    },
+    // 取得首頁訊息
+    getHomeNewsList() {
+      let queryData = {
+        count: 10,
+        page: 1,
+      };
+      AjaxService.post(
+        "/server/news/range",
+        queryData,
         (successResp) => {
-          console.log(successResp);
+          if (successResp.restData) {
+            let resultList = successResp.restData;
+            console.log(resultList);
+            this.newsList = resultList.map((item) => {
+              let news = {
+                newsType: item.type,
+                newsTitle: item.title,
+              };
+              return news;
+            });
+            console.log("查詢特定範圍內的訊息成功!");
+          }
         },
         (errorResp) => {
-          console.log("error");
+          console.log("查詢特定範圍內的訊息失敗!");
           console.log(errorResp);
         }
       );
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -29,38 +29,40 @@
     <!-- content below -->
     <!-- category img 四個圈圈 -->
     <div class="category section bg-primary-light">
-      <b-row class="content" align-v="center" align-h="center">
-        <template v-for="item of categoryImg">
-          <b-col cols="6" md="3" :key="item.index">
-            <router-link :to="item.link">
-              <b-img-lazy class="img shadow-lg rounded" rounded="circle" blank-color="#444422" center fluid :src="require(`@/assets/img/` + item.pic)" />
-            </router-link>
-            <p class="text-center text-white mt-2">{{ item.label }}</p>
-          </b-col>
-        </template>
-      </b-row>
+      <b-container>
+        <b-row class="content" align-v="center" align-h="center">
+          <template v-for="item of categoryImg">
+            <b-col cols="6" md="3" :key="item.index">
+              <router-link :to="item.link">
+                <b-img-lazy class="img shadow-lg rounded" rounded="circle" blank-color="#444422" center fluid :src="require(`@/assets/img/` + item.pic)" />
+              </router-link>
+              <p class="text-center text-white mt-2">{{ item.label }}</p>
+            </b-col>
+          </template>
+        </b-row>
+      </b-container>
     </div>
     <!-- 訊息專區 -->
     <div class="section">
       <b-container>
         <b-row class="msg-area" align-v="center" align-h="center">
           <!-- 訊息 -->
-          <b-col md="7">
-            <template v-for="(item, index) of msgArr">
-              <!-- 預設只查詢出五筆 -->
+          <b-col md="6" class="pl-5">
+            <template v-for="(news, index) of newsList">
               <div :key="index">
-                <router-link :to="item.link">
-                  <div class="d-inline-flex">
-                    <div class="text-primary mr-2 mb-2 mb-lg-3">{{ item.title }}</div>
-                    <div class="text-primary">{{ item.content }}</div>
+                <router-link :to="`${'/news/' + news.id}`">
+                  <div class="d-inline-flex align-items-center text-primary mb-3">
+                    <span class="mr-2">【{{ news.newsType }}】</span>
+                    <b-icon class="mr-2" icon="caret-right-fill" style="width: 13px;" />
+                    <span>{{ news.newsTitle }}</span>
                   </div>
                 </router-link>
               </div>
             </template>
           </b-col>
           <!-- 圖片 -->
-          <b-col md="3" class="text-center mt-5 mt-md-0">
-            <h4 class="text-primary-dark fz-title-middle mb-6">訊息專區</h4>
+          <b-col md="4" class="text-center mt-6 mt-md-0">
+            <h4 class="text-primary-dark fz-title-middle mb-5">訊息專區</h4>
             <b-img class="msg-img" :src="require(`@/assets/img/other/message.png`)" />
           </b-col>
         </b-row>
@@ -70,11 +72,11 @@
     <div class="section bg-primary">
       <b-container>
         <b-row class="location" align-v="center" align-h="center">
-          <b-col cols="4" class="text-center pt-5 pb-4 pb-md-5">
+          <b-col md="4" class="text-center pt-5 pb-5">
             <h4 class="text-white fz-title-middle mb-5">餐廳位置</h4>
             <b-img-lazy class="img" :src="require(`@/assets/img/other/location.png`)" />
           </b-col>
-          <b-col cols="7" class="text-center">
+          <b-col md="7" class="text-center">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.1233455472952!2d121.52087371484247!3d25.063808183958436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a9448b78d849%3A0x5d8ebd86f1e02e1f!2z57SX5rGA5aic5aW96aOf!5e0!3m2!1szh-TW!2stw!4v1636878580396!5m2!1szh-TW!2stw"
               class="map"
@@ -87,14 +89,14 @@
     </div>
     <!-- 營業時間 -->
     <div class="section open-time">
-      <div class="text-primary-dark">
-        <b-row align-v="center" align-h="center">
+      <b-container>
+        <b-row class="text-primary-dark" align-v="center" align-h="center">
           <!-- 圖片 -->
-          <b-col md="6" class="text-center mb-5 mb-md-0">
+          <b-col md="7" class="text-center mb-5 mb-md-0">
             <b-img-lazy class="img" :src="require(`@/assets/img/restaurant/view01.jpg`)" />
           </b-col>
           <!-- 文字 -->
-          <b-col md="4" class="ml-5 ml-md-0">
+          <b-col md="5" class="pl-4 pl-md-0 ml-5 ml-md-0">
             <div class="fz-title-middle mb-3">營業時間 <b-icon icon="clock" /></div>
             <div class="mb-2">星期一 ~ 星期五 &nbsp; 11:00 ~ 20:30</div>
             <div class="mb-5">星期六 ~ 星期日 &nbsp; 10:30 ~ 21:30</div>
@@ -108,7 +110,7 @@
             <!-- <div class="mt-3"><b-button variant="primary-darker" @click="serverTest()">測試按鈕</b-button></div> -->
           </b-col>
         </b-row>
-      </div>
+      </b-container>
     </div>
   </div>
 </template>
@@ -177,19 +179,29 @@
 // 餐廳位置
 .location {
   .img {
-    height: 55%;
-    width: 55%;
+    width: 35%;
+    @include media-breakpoint-up(md) {
+      width: 60%;
+    }
   }
   .map {
-    width: 600px;
-    height: 400px;
+    width: 65vw;
+    height: 50vh;
     border: none;
+    @include media-breakpoint-up(md) {
+      width: 40vw;
+      height: 50vh;
+    }
+    @include media-breakpoint-up(xl) {
+      width: 35vw;
+      height: 50vh;
+    }
   }
 }
 // 營業時間
 .open-time {
   .img {
-    width: 90%;
+    width: 75%;
   }
   .icon {
     height: 30px;

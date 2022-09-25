dc7c1d06ed35ab7bcd8e23c454c5edeb0e463be8
YC <saiyuki0804@gmail.com>
2022-09-25 17:37:44 +0800

    [YC] 測試部署，上傳dist

---
+++ dist/js/chunk-745dbe27.c0251ecd.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-745dbe27"],{"440f":function(e,t,n){"use strict";n.r(t);var r=function(){var e=this,t=e.$createElement,r=e._self._c||t;return r("div",[r("global-title",{attrs:{title:"訊息專區",overlay:"#466050",image:n("b7ed")}}),r("b-container",[r("router-view")],1)],1)},a=[],i={name:"News"},o=i,l=n("2877"),s=Object(l["a"])(o,r,a,!1,null,"35643216",null);t["default"]=s.exports},b7ed:function(e,t,n){e.exports=n.p+"img/news-hero.abfe0b46.jpg"}}]);
+//# sourceMappingURL=chunk-745dbe27.c0251ecd.js.map
